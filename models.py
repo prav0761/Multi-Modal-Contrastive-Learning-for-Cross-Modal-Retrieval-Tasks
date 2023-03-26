24cb4573b2e7a500a0f99818e87e0576c9dc1bb6
Praveen <93844635+prav0761@users.noreply.github.com>
2023-03-26 14:40:19 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,137 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+class ResNetSimCLR(nn.Module):
+    def __init__(self, model='resnet18', projection_dim=128, layers_to_train=['layer4']):
+        """
+        Initializes ResNetSimCLR model.
+
+        Parameters:
+        - model: str, the ResNet model to use (default: 'resnet18')
+        - projection_dim: int, the dimension of the projection head output (default: 128)
+        - layers_to_train: list of str, the names of the layers in the ResNet model to train (default: ['layer4'])
+        """
+        super(ResNetSimCLR, self).__init__()
+
+        # Instantiate the backbone ResNet model
+        if model == 'resnet18':
+            backbone = models.resnet18(pretrained=True)
+            in_features = 512
+        elif model == 'resnet50':
+            backbone = models.resnet50(pretrained=True)
+            in_features = 2048
+        elif model == 'resnet101':
+            backbone = models.resnet101(pretrained=True)
+            in_features = 2048
+        else:
+            raise ValueError('Unsupported ResNet model:', model)
+
+        # Freeze the layers that are not specified in layers_to_train
+        for name, child in backbone.named_children():
+            if name not in layers_to_train:
+                for param in child.parameters():
+                    param.requires_grad = False
+
+        # Remove last fully-connected layer from the backbone
+        self.backbone = nn.Sequential(*list(backbone.children())[:-1])
+
+        # Define the transform to be applied to input images
+        self.transform = transforms.Compose([
+                            transforms.Resize(256),
+                            transforms.CenterCrop(224),
+                            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                        ])
+
+        # Add the projection head layers
+        self.projection_head = nn.Sequential(
+            nn.Linear(in_features, in_features),
+            nn.ReLU(),
+            nn.Linear(in_features, projection_dim)
+        )
+
+    def forward(self, x):
+        """
+        Performs a forward pass through the ResNetSimCLR model.
+
+        Parameters:
+        - x: tensor of shape (batch_size, 3, height, width), the input images
+
+        Returns:
+        - features: tensor of shape (batch_size, in_features), the features extracted from the backbone
+        - projection: tensor of shape (batch_size, projection_dim), the projections of the features
+        """
+        # Apply the transform to the input images
+        x = self.transform(x)
+
+        # Extract features from the backbone
+        x = x.unsqueeze(0) # Add batch dimension
+        features = self.backbone(x)
+
+        # Flatten the features and pass them through the projection head
+        features = features.view(features.size(0), -1)
+        projection = self.projection_head(features)
+
+        # Return the features and projections
+        return features, projection
+class TextEncoder(nn.Module):
+    def __init__(self, model='openai-gpt', projection_dim=128, layers_to_train=['h.11']):
+        """
+        A PyTorch module for text encoding using a pre-trained OpenAI GPT model.
+
+        Args:
+            model (str): The name or path of the pre-trained OpenAI GPT model to use. Defaults to 'openai-gpt'.
+            projection_dim (int): The dimension of the projection head output. Defaults to 128.
+            layers_to_train (list of str): The names of the layers to train. Defaults to ['h.11'].
+        """
+        super(TextEncoder, self).__init__()
+
+        # Load backbone and tokenizer
+        self.backbone = OpenAIGPTModel.from_pretrained(model)
+        self.config = self.backbone.config
+        self.tokenizer = OpenAIGPTTokenizer.from_pretrained(model)
+        
+        # Set requires_grad for each parameter based on layers_to_train
+        for name, param in self.backbone.named_parameters():
+            if any(name.startswith(prefix) for prefix in layers_to_train):
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+        # Projection head
+        self.projection_head = nn.Sequential(
+            nn.Linear(self.config.n_embd, self.config.n_embd),
+            nn.ReLU(),
+            nn.Linear(self.config.n_embd, projection_dim)
+        )
+
+    def forward(self, text):
+        """
+        Forward pass of the TextEncoder module.
+
+        Args:
+            text (str): The input text to encode.
+
+        Returns:
+            A tuple containing the encoded text features and their projections.
+        """
+        # Tokenize input text
+        input_ids = self.tokenizer.tokenize(text)
+        indexed_tokens = self.tokenizer.convert_tokens_to_ids(input_ids)
+        tokens_tensor = torch.tensor([indexed_tokens])
+        
+        # Get text features from backbone
+        features = self.backbone(tokens_tensor)[:, 0, :]
+
+        # Pass text features through projection head
+        projections = self.projection_head(features)
+
+        return features, projections
+
---
+++ utils.py
@@ -0,0 +1,51 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import torch
+import torchvision
+from torchvision import transforms
+from PIL import Image
+import requests
+import io
+from io import BytesIO
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+import torchvision.models as models
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import os
+from torch.utils.data import Dataset
+import random
+from tqdm import tqdm
+import json
+import threading
+import torchvision.transforms.functional as F
+import torchvision.models as models
+import torch.nn as nn
+
+
+def get_stats():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    print('No of GPUs i have is',torch.cuda.device_count())
+    print(torch.cuda.current_device())
+    print('My Graphic Card is',torch.cuda.get_device_name(torch.cuda.current_device()))
+    print('Is Cuda Available',torch.cuda.is_available())
+def layerwise_trainable_parameters(model):
+    total_trainable_params = 0
+    for name, param in model.named_parameters():
+        num_trainable_params = 0
+        if param.requires_grad:
+            num_trainable_params = param.numel()
+            total_trainable_params += num_trainable_params
+        print(f'Layer name: {name}\tNumber of trainable parameters: {num_trainable_params}')
+    print(f'Total number of trainable parameters: {total_trainable_params}')
+def count_trainable_parameters(model):
+    total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return total_params
+
